93a706d98227ce92a4c1e4fa0e5c3572c0c3681e
ruaaaldahham209 <66695348+ruaaaldahham209@users.noreply.github.com>
2020-07-04 17:26:03 +0300

    مادري ليش استصعبته

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,5 +1,15 @@
 // قم بإنشاء الدوال التالية
 
+
+
+    
+
+
+
+
+
+
+
 /*
  * greet(name) اسم الدالة
  * - name قد تستقبل وقد لا تستقبل
@@ -7,11 +17,32 @@
  * - name إن استقبلت اسم أو "Hello <name>" تطبع
  */
 
-function greet(name) {}
+function greet(name) {
+let message = "hello";
+if (name){
+    message==name;
+    console.log(message)
+}else{
+    console.log(message);
+}
+}
+greet();
+greet("ruaa");
+
 
 /*
  * isOdd(n) اسم الدالة
  * - n تستقبل رقم
  * - عدا ذلك false إن كان الرقم فردياً وترجع true ترجع القيمة
  */
-function isOdd(n) {}
+function isOdd(n) {
+    if (n % 2 == 1){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+console.log (isOdd(5));
+console.log (isOdd(3));
